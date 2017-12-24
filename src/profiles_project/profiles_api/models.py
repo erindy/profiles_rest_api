f32c3a62808ab5f5ab2806a5e14d49233225dd79
erindy <erindmehmeti@gmail.com>
2017-12-24 17:35:44 +0100

    Added model for storing the model feed items

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -64,6 +64,19 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         return self.email
 
 
+class ProfileFeedItem(models.Model):
+    """Profile status updates."""
+
+    user_profile = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """return the model as a string"""
+
+        return self.status_text
+
+
 
 
 
